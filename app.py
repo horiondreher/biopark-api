778705a00ce6319c14fc4f514c410739d141af48
Horion Dreher <horiondreher@gmail.com>
2020-11-15 02:39:51 -0300

    Config inicial de app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
+from flask_restful import Api
 
 app = Flask(__name__)
+app.secret_key = 'biopark_key'
+api = Api(app)
 
-@app.route('/')
-def home():
-    return "Hello, biopark!"
-
-app.run(port = 5000)
+app.run(port = 5000, debug = True)
