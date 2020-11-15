2b5655ae2318174db883d27a6614afe717bc11a7
Horion Dreher <horiondreher@gmail.com>
2020-11-15 00:52:48 -0300

    Criaçao de BD e tabelas

--- create_db.py
+++ create_db.py
@@ -1,6 +1,37 @@
 import mysql.connector
 from mysql.connector import errorcode
 
+DB_NAME = 'messages'
+TABLES = {}
+
+TABLES['employee'] = (
+    " CREATE TABLE users ( "
+    " worker_id int(16) unsigned NOT NULL, "
+    " name varchar(32) NOT NULL, "
+    " password varchar(32) NOT NULL, "
+    " email varchar(32) NOT NULL, "
+    " PRIMARY KEY (worker_id), "
+    " UNIQUE KEY email (email) "
+    " ) ENGINE=MyISAM"
+)
+
+TABLES['messages'] = (
+    " CREATE TABLE messages ( "
+    " dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+    " message_id int(16) unsigned NOT NULL, "
+    " message_dt DATETIME NOT NULL, "
+    " sender_worker_id int(16) NOT NULL, "
+    " receiver_worker_id int(16) NOT NULL, "
+    " email varchar(32) DEFAULT NULL, "
+    " phone varchar(32) DEFAULT NULL, "
+    " message varchar DEFAULT NULL, "
+    " message_app varchar DEFAULT NULL, "
+    " PRIMARY KEY (message_id), "
+    " UNIQUE KEY email (email), "
+    " FOREIGN KEY (sender_worker_id) REFERENCES users(worker_id) "
+    " ) ENGINE=MyISAM"
+)
+
 # configurações para o BD de teste
 config = {
     'user': 'root',
@@ -12,6 +43,7 @@ config = {
 # utilizando o mysql.connector, passo as informações necessárias para conexão local ao MySQL
 try:
     cnx = mysql.connector.connect (**config)
+    cursor = cnx.cursor()
     print("Conexão realizada com sucesso")
 except mysql.connector.Error as err: # tratamento de erros com códigos disponibilizados por "errorcode"
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -22,3 +54,24 @@ except mysql.connector.Error as err: # tratamento de erros com códigos disponib
         print(err)
 else:
     cnx.close()
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Falha na criação do banco de dados: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("O Banco de dados {} não existe".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("O Banco de dados {} foi criado com sucesso".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+        
